6a8ce92db4dec02837b7e9ed53edcafc7fff748e
Thomas Croft <thomas.croft@proton.me>
2024-08-15 12:32:27 -0600

    Added: nixConfig

--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   description = "A nix flake to pull Nightly WezTerm, build it, and push it to cachix.";
 
+  nixConfig = {
+    extra-substituters = "https://wezterm-nightly.cachix.org";
+    extra-trusted-public-keys = ["wezterm-nightly.cachix.org-1:zsTr51TeTCRg+bHwUr0KfW3XIIb7Avisrj/hXwVzC2c="];
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     systems.url = "systems";
